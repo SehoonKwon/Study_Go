b0cee84e0ed64ae94e92f37d09b9602873471be4
SehoonKwon <49121446+SehoonKwon@users.noreply.github.com>
2021-11-13 10:35:31 +0900

    ver2
    export와 variable 학습

--- learnGo/main.go
+++ learnGo/main.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello wolrd")
+
+	//아래는 동일한 문법이다  : 키워드가 auto 인듯
+	var name string = "Sehoon"
+	fmt.Println(name)
+
+	name2 := "Sehoon2"
+	fmt.Println(name2)
 }
